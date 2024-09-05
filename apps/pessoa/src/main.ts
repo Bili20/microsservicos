bd57ac580610dc4c74b99cf189ae3fb9e1c449ba
gabriel milosz cruz <gbrlmilosz@gmail.com>
2024-09-05 14:04:56 -0300

    Update main.ts

--- apps/pessoa/src/main.ts
+++ apps/pessoa/src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
+import { PessoaModule } from './pessoa.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(PessoaModule);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
