234ea7f79871aba2424599b0f462507f943bae48
gabriel milosz cruz <gbrlmilosz@gmail.com>
2024-09-05 14:04:54 -0300

    Delete app.module.ts

--- apps/pessoa/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { PessoaModule } from './useCase/pessoa.module';
-import { typeOrmConfig } from './config/typeormConfig';
-
-@Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), PessoaModule],
-})
-export class AppModule {}
